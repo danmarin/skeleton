f2c72e772e6a720de6185ab49aa482c3cca54ba8
Daniel Marin <dan@spiral8.net>
2015-03-21 13:38:21 +0200

    removed session debug from template file

--- templates/default/user/index.php
+++ templates/default/user/index.php
@@ -5,9 +5,6 @@
 		<div class="col-md-12">
 			<h1>This is the membership area</h1>
 			<p><a href="<?=$this->url['user_logout_url']?>">Logout</a></p>
-			<pre>
-			<?php print_r($_SESSION) ?>
-			</pre>
 		</div>
 	</div>
 </div>
