f5b7f8d8f318bc4291e2ccdbb3e93fad2281f355
keen <dan@spiral8.net>
2024-11-19 16:51:51 +0200

    Some small updates added and removed the vendor folder

--- templates/default/admin/login/footer.php
+++ templates/default/admin/login/footer.php
@@ -1,4 +1,4 @@
-<script src="<?=$this->url['assets_url']?>/js/jquery-2.1.3.min.js"></script>
+<script src="<?=$this->url['assets_url']?>/js/jquery-3.0.0.min.js"></script>
 <script src="<?=$this->url['assets_url']?>/js/bootstrap.min.js"></script>
 
 </body>
